f30bc8086f7dd4fae765063876dc8a0eaec7f127
unknown <mattgvo@gmail.com>
2019-04-30 17:12:32 -0600

    ready now

--- db/create_house_table.sql
+++ db/create_house_table.sql
@@ -10,50 +10,62 @@ CREATE TABLE houses (
     rent DECIMAL
 );
 
--- INSERT INTO houses (
---     name,
---     address,
---     city,
---     state,
---     zip,
---     img
--- ) VALUES (
---     'Goondocks HQ',
---     '38 GoonDocks Blvd',
---     'Astoria',
---     'OR',
---     1985,
---     'https://img.huffingtonpost.com/asset/55d5ed831700006e0056810d.jpeg?cache=md3f11maSu&ops=1910_1000'
---  );  
+INSERT INTO houses (
+    name,
+    address,
+    city,
+    state,
+    zip,
+    img,
+    mortgage,
+    rent
+) VALUES (
+    'Goondocks HQ',
+    '38 GoonDocks Blvd',
+    'Astoria',
+    'OR',
+    1985,
+    'https://img.huffingtonpost.com/asset/55d5ed831700006e0056810d.jpeg?cache=md3f11maSu&ops=1910_1000',
+    2500,
+    3125
+ );  
 
--- INSERT INTO houses (
---     name,
---     address,
---     city,
---     state,
---     zip,
---     img
--- ) VALUES (
---     'Hillard Residence',
---     '25 Fruiting Drive',
---     'San Francisco',
---     'CA',
---     1993,
---     'https://cdn-images-1.medium.com/max/1200/0*eipmLIa4xVGLxew1.png'
--- );
+INSERT INTO houses (
+    name,
+    address,
+    city,
+    state,
+    zip,
+    img,
+    mortgage,
+    rent
+) VALUES (
+    'Hillard Residence',
+    '25 Fruiting Drive',
+    'San Francisco',
+    'CA',
+    1993,
+    'https://cdn-images-1.medium.com/max/1200/0*eipmLIa4xVGLxew1.png',
+    1400,
+    1700
+);
 
--- INSERT INTO houses (
---     name,
---     address,
---     city,
---     state,
---     zip,
---     img
--- ) VALUES (
---     'Tanner House',
---     '48 Full Rd',
---     'San Francisco',
---     'CA',
---     1987,
---     'https://hoodline.imgix.net/uploads/story/image/25350/Tanner-family-Victorian-1882-Girard-San-Francisco-Full-House.jpg?auto=format'
--- );
+INSERT INTO houses (
+    name,
+    address,
+    city,
+    state,
+    zip,
+    img,
+    mortgage,
+    rent
+) VALUES (
+    'Tanner House',
+    '48 Full Rd',
+    'San Francisco',
+    'CA',
+    1987,
+    'https://hoodline.imgix.net/uploads/story/image/25350/Tanner-family-Victorian-1882-Girard-San-Francisco-Full-House.jpg?auto=format',
+    2500,
+    3125
+);
--- src/Components/House/House.js
+++ src/Components/House/House.js
@@ -3,17 +3,19 @@ import './House.css'
 
 export default class House extends Component {
     render(){
-        const {id, name, address,city,state,zip,img} = this.props.house
+        const {id, name, address,city,state,zip,img,mortgage,rent} = this.props.house
         const {deleteHouse} = this.props
         return(
             <div className="House">
                 <div onClick={deleteHouse} id={id} className="X">X</div>
+                <img src={img} alt={name}/>
                 <p>Property Name: {name}</p>
                 <p>Address: {address}</p>
                 <p>City: {city}</p>
                 <p>State: {state}</p>
                 <p>Zip:{zip}</p>
-                <img src={img} alt={name}/>
+                <p>Monthly Mortgage: ${mortgage}</p>
+                <p>Desired Rent: ${rent}</p>
             </div>
         )
     }
